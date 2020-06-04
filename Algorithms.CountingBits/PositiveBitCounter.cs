31e75ad9974542b3e7d9966fd5760a7c2558873a
Arturo Iriarte <arturo_iriarte@outlook.com>
2020-06-04 21:35:56 +0200

    Positive bit counter using linq

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -6,12 +6,34 @@ namespace Algorithms.CountingBits
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class PositiveBitCounter
     {
+        /* Using linq to reverse the binary string
+         * And then looping through the string to take
+         * the 1-bits position and finally count them
+         */
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0)
+                throw new ArgumentException("The input must be a positive value");
+
+            if (input == 0)
+                return new List<int> { 0 };
+
+            List<int> list = new List<int>();
+            var bits = Convert.ToString(input, 2).Reverse();
+
+            for (int i = 0, len = bits.Count(); i < len; i++)
+            {
+                if (bits.ElementAt(i) == '1')
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list.Prepend(list.Count);
         }
     }
 }
